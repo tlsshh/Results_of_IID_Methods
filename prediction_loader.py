fa10bab5e0fe6b00cf37398c8625b9d673a80243
luojundan <598991258@qq.com>
2022-03-11 15:05:26 +0000

    implement PredictionLoader
    .

--- prediction_loader.py
+++ prediction_loader.py
@@ -12,6 +12,11 @@ class PredictionLoader(ABC):
         '''Please implement in subclass'''
         raise NotImplemented
 
+    @abstractmethod
+    def get_pred_rs_img_path(self, id):
+        '''Please implement in subclass'''
+        raise NotImplemented
+
 
 class Li_2018_CGI_Loader(PredictionLoader):
     def __init__(self, dir):
@@ -30,11 +35,17 @@ class Li_2018_CGI_Loader(PredictionLoader):
         hdf5_file_read.close()
         return pred_R
 
+    def get_pred_rs_img_path(self, id):
+        r_img_path = os.path.join(self.image_dir, f"{id}-r.png")
+        s_img_path = os.path.join(self.image_dir, f"{id}-s.png")
+        return r_img_path, s_img_path
+
 
 class Luo_2020_NIID_Net_Loader(PredictionLoader):
     def __init__(self, dir):
         self.dir = dir
         self.raw_dir = os.path.join(self.dir, "final_raw")
+        self.image_dir = os.path.join(self.dir, "SAW_pred_imgs")
 
     def get_pred_r(self, id, space):
         assert space in ["srgb"]
@@ -42,3 +53,77 @@ class Luo_2020_NIID_Net_Loader(PredictionLoader):
         pred_R = np.load(pred_R_path).astype(np.float32)
         pred_R = util.rgb_to_srgb(pred_R)
         return pred_R
+
+    def get_pred_rs_img_path(self, id):
+        r_img_path = os.path.join(self.image_dir, f"{id}_R.png")
+        s_img_path = os.path.join(self.image_dir, f"{id}_S.png")
+        return r_img_path, s_img_path
+
+
+class CRefNet(PredictionLoader):
+    def __init__(self, dir):
+        self.dir = dir
+        self.image_dir = os.path.join(self.dir, "split")
+
+    def get_pred_r(self, id, space):
+        '''Please implement in subclass'''
+        raise NotImplemented
+
+    def get_pred_rs_img_path(self, id):
+        r_img_path = os.path.join(self.image_dir, f"{id}_r.jpg")
+        s_img_path = os.path.join(self.image_dir, f"{id}_s.jpg")
+        return r_img_path, s_img_path
+
+
+class Wang_2019_Discriminative_Loader(PredictionLoader):
+    def __init__(self, dir):
+        self.dir = dir
+        self.image_dir = os.path.join(self.dir, "test-imgs_ep12_results")
+
+    def get_pred_r(self, id, space):
+        '''Please implement in subclass'''
+        raise NotImplemented
+
+    def get_pred_rs_img_path(self, id):
+        r_img_path = os.path.join(self.image_dir, f"{id}_r.png")
+        s_img_path = os.path.join(self.image_dir, f"{id}_sr.png")
+        return r_img_path, s_img_path
+
+
+class Bi_2015_L1smoothing_Loader(PredictionLoader):
+    def __init__(self, dir):
+        self.dir = dir
+        self.image_dir = os.path.join(self.dir, "our_result")
+
+    def get_pred_r(self, id, space):
+        '''Please implement in subclass'''
+        raise NotImplemented
+
+    def get_pred_rs_img_path(self, id):
+        r_img_path = os.path.join(self.image_dir, f"{id}-r.png")
+        s_img_path = os.path.join(self.image_dir, f"{id}-s.png")
+        return r_img_path, s_img_path
+
+
+class General_Loader(PredictionLoader):
+    def __init__(self, dir):
+        self.dir = dir
+        self.image_dir = self.dir
+
+    def get_pred_r(self, id, space):
+        '''Please implement in subclass'''
+        raise NotImplemented
+
+    def get_pred_rs_img_path(self, id):
+        r_img_path = os.path.join(self.image_dir, f"{id}-r.png")
+        s_img_path = os.path.join(self.image_dir, f"{id}-s.png")
+        return r_img_path, s_img_path
+
+
+class InputLoader(object):
+    def __init__(self, dir):
+        self.dir = dir
+        self.data_dir = os.path.join(self.dir, "data")
+
+    def get_input_img_path(self, id):
+        return os.path.join(self.data_dir, f"{id}.png")
